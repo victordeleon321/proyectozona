bf2d1988b4635e70835a41f45517870b1fc16ff3
victordeleon321 <54379420+victordeleon321@users.noreply.github.com>
2019-11-09 09:57:24 -0600

    Update models.py

--- torneo/models.py
+++ torneo/models.py
@@ -6,7 +6,6 @@ class Personaje(models.Model):
     nombre = models.CharField(max_length=30,default='')
     compania = models.CharField(max_length=30,default='')
     anio = models.IntegerField()
-    imagen = models.ImageField(default='')
 
     def __str__(self):
         return self.nombre
